e7bc2796ef567e47b0597ac168ac1ab7653ca236
Nazar <nazarostryzhniuk@gmail.com>
2015-05-04 13:18:20 +0300

    context starts, index page works

--- src/main/java/ostryzhniukn/JerseyWithMVC/config/ServletInitializer.java
+++ src/main/java/ostryzhniukn/JerseyWithMVC/config/ServletInitializer.java
@@ -1,7 +1,42 @@
 package ostryzhniukn.JerseyWithMVC.config;
 
+import org.springframework.util.ClassUtils;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.DelegatingFilterProxy;
+import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
 /**
  * Created by nazar on 5/3/15.
  */
-public class ServletInitializer {
+public class ServletInitializer extends AbstractDispatcherServletInitializer {
+
+    @Override
+    protected WebApplicationContext createServletApplicationContext() {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+//        context.scan(ClassUtils.getPackageName(getClass()));
+        context.register(WebMvcConfig.class);
+        return context;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+
+    @Override
+    protected WebApplicationContext createRootApplicationContext() {
+        return null;
+    }
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        super.onStartup(servletContext);
+//        DelegatingFilterProxy filter = new DelegatingFilterProxy("springSecurityFilterChain");
+//        filter.setContextAttribute("org.springframework.web.servlet.FrameworkServlet.CONTEXT.dispatcher");
+//        servletContext.addFilter("springSecurityFilterChain", filter).addMappingForUrlPatterns(null, false, "/*");
+    }
 }
--- src/main/java/ostryzhniukn/JerseyWithMVC/controller/IndexController.java
+++ src/main/java/ostryzhniukn/JerseyWithMVC/controller/IndexController.java
@@ -3,5 +3,9 @@ package ostryzhniukn.JerseyWithMVC.controller;
 /**
  * Created by nazar on 5/3/15.
  */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class IndexController {
 }
